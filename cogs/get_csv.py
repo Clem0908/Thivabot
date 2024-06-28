882ef27e47222e4336d94cd3f9b8738969659b5c
Clem0908 <clementlyonnet@hotmail.fr>
2024-06-28 11:49:52 +0200

    Arguments optionels get_csv

--- cogs/get_csv.py
+++ cogs/get_csv.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import datetime
 
 class Get_csv(commands.Cog):
 
@@ -8,8 +9,18 @@ class Get_csv(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def get_csv(self,ctx,month: str, year: str):
-        await ctx.send(file=discord.File("./database/inactifs-"+month+year+".csv"))
+    async def get_csv(self,ctx,*args):
+
+        if len(args) == 2:
+            await ctx.send(file=discord.File("./database/inactifs-"+str(args[0])+str(args[1])+".csv"))
+            return
+        if len(args) == 0:
+            dt = datetime.datetime.today()
+            await ctx.send(file=discord.File("./database/inactifs-"+str(dt.month)+str(dt.year)+".csv"))
+            return
+
+        await ctx.send("Erreur de syntaxe : !T get_csv | !T get_csv 6 2024")
+        return
 
 async def setup(bot):
     await bot.add_cog(Get_csv(bot))
